757c8335865934b4bf8aa8817226053cef1efdbb
D3NCyan <edwarddoltons@gmail.com>
2021-10-22 11:51:53 +0800

    configuration material ui

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,16 +1,32 @@
 import './Header.scss'
 import { showLoginModal, showRegisterModal } from '../../../utils'
+import { useSelector } from 'react-redux'
+import { ProfileDefault } from '../../../assets'
 
 const Header = ({logo}) => {
-    return (
-        <header className="d-flex-between">
-            <img src={logo} alt="this is logo" />
-            <div className="section-button__header">
-                <button className="btn-login" id="btnLogin" onClick={showLoginModal}>Login</button>
-                <button className="btn-warning ml-m" id="btnRegister" onClick={showRegisterModal}>Register</button>
-            </div>
-        </header>
-    )
+    const isLoginSession = useSelector(state => state.isLogin)
+    if (isLoginSession) {
+        return (
+            <header className="d-flex-between">
+                <img src={logo} alt="this is logo" />
+                <div className="section-button__header">
+                    <div className="profile">
+                        <img src={ProfileDefault} alt="" />
+                    </div>
+                </div>
+            </header>
+        )
+    } else {
+        return (
+            <header className="d-flex-between">
+                <img src={logo} alt="this is logo" />
+                <div className="section-button__header">
+                    <button className="btn-login" id="btnLogin" onClick={showLoginModal}>Login</button>
+                    <button className="btn-warning ml-m" id="btnRegister" onClick={showRegisterModal}>Register</button>
+                </div>
+            </header>
+        )
+    }
 }
 
 
--- src/components/molecules/Modal/index.js
+++ src/components/molecules/Modal/index.js
@@ -1,39 +1,89 @@
 import './Modal.scss'
-// import { closeModal } from '../../../utils'
+import * as React from 'react';
+
 import { Gap, Input } from '../../atoms'
 import { IconPalm2, IconHibicus2 } from '../../../assets'
 import { closeLoginModal, closeRegisterModal } from '../../../utils'
+import store from '../../../store'
+
+import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import IconButton from '@mui/material/IconButton';
 
 const ModalLogin = () => {
-    return (
-        <div className="modal" id="modal-login">   
-            <div className="heading-modal">
-                <img className="icon-palm__login" src={IconPalm2} alt="" />
-                <img className="icon-hibicus__login" src={IconHibicus2} alt="" />
-            </div>
-            <div className="content-modal">
-                <form onSubmit={function(e) {
-                            e.preventDefault()
+    const [open, setOpen] = React.useState(false);
+    const [message, setMessage] = React.useState('Not found')
+    
+    const handleClick = () => {
+        setOpen(true)
+    }
+    const handleClose = () => {
+        setOpen(false)
+    }
+
+    const action = (
+        <>
+            <Button color="secondary" size="small" onClick={handleClose}>
+                CLOSE
+            </Button>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+            </IconButton>
+        </>
+    );
+
+    function loginSession(event) {
+        event.preventDefault()
+
                             
-                            const dataUser = JSON.parse(window.localStorage.getItem('user'))
-                            if (dataUser.email === e.target.email.value && dataUser.password === e.target.password.value) {
-                                console.log(true)
-                            } else {
-                                console.log(false)
-                            }
-                        }
-                    }>
-                    <p className="title">Login</p>
-                    <Input label="Email" fontSize={24} name="email" required />
-                    <Gap height={20} />
-                    <Input label="Password" fontSize={24} name="password" required />
-                    <Gap height={20} />
-                    <button className="btn-warning full">Login</button>
-                    <Gap height={23} />
-                    <p className="text-center disclamer">Don't have an account? Klik Here</p>
-                </form>
+        const dataUser = JSON.parse(window.localStorage.getItem('user'))
+        if (dataUser.email === event.target.email.value && dataUser.password === event.target.password.value) {
+            store.dispatch({ type: 'LOGIN' })
+            setMessage('Login Success')
+        } else {
+            store.dispatch({ type: 'NOT_LOGIN'})
+            setMessage('Email or password are incorrect')
+        }
+    }
+
+    return (
+        <>
+            <div className="modal" id="modal-login">  
+                <div className="heading-modal">
+                    <img className="icon-palm__login" src={IconPalm2} alt="" />
+                    <img className="icon-hibicus__login" src={IconHibicus2} alt="" />
+                </div>
+                <div className="content-modal">
+                    <form onSubmit={loginSession}>
+                        <p className="title">Login</p>
+                        <Input label="Email" fontSize={24} name="email" required />
+                        <Gap height={20} />
+                        <Input label="Password" fontSize={24} name="password" required />
+                        <Gap height={20} />
+                        <button className="btn-warning full" onClick={handleClick}>Login</button>
+                        <Gap height={23} />
+                        <p className="text-center disclamer">Don't have an account? Klik Here</p>
+                    </form>
+                </div>
             </div>
-        </div>
+
+            <Snackbar sx={{
+                position: 'fixed',
+                bottom: 0,
+                zIndex: 999999999,
+                transform: 'translate(50px, -25px) scale(1.2)'
+            }}
+                open={open}
+                autoHideDuration={6000}
+                onClose={handleClose}
+                message={message}
+                action={action}
+            />
+        </>
     )
 }
 
