10f0dd26b27a9299e98767dc971765458b0e7662
D3NCyan <edwarddoltons@gmail.com>
2021-10-22 11:52:36 +0800

    refactor global state

--- src/store.js
+++ src/store.js
@@ -1,16 +1,9 @@
 import { createStore } from 'redux'
 import rootReducer from './rootReducer'
 
-const store = createStore(rootReducer, 0)
+const store = createStore(rootReducer, false)
 store.subscribe(() =>{
     console.log('Current state', store.getState())
-})
-
-store.dispatch({
-    type: 'INCREMENT'
-})
-store.dispatch({
-    type: 'INCREMENT'
 })
 
 export default store
