11c48c32adc56b8c07337e8879de728c57ce9063
D3NCyan <edwarddoltons@gmail.com>
2021-10-23 11:40:50 +0800

    add configuration of muibutton

---
+++ src/utils/CustomButton/MuiButton.js
@@ -0,0 +1,15 @@
+const muiButton = {
+    backgroundColor: '#FFAF00',
+    '&:hover': {
+        backgroundColor: '#e7bb5a'
+    }
+}
+
+const floatRight = {
+    backgroundColor: '#FFAF00',
+    '&:hover': {
+        backgroundColor: '#e7bb5a'
+    },
+}
+
+export { muiButton, floatRight }
