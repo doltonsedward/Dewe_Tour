24ea72fa5e5dfbe0196d8592bcb7bbf74940879b
D3NCyan <edwarddoltons@gmail.com>
2021-10-24 10:38:18 +0800

    add paymentButton for muiButton

--- src/utils/CustomButton/MuiButton.js
+++ src/utils/CustomButton/MuiButton.js
@@ -12,4 +12,37 @@ const floatRight = {
     },
 }
 
-export { muiButton, floatRight }
+const paymentButton = {
+    width: '213px',
+    height: '50px',
+    backgroundColor: '#FFAF00',
+    '&:hover': {
+        backgroundColor: '#e7bb5a'
+    }
+}
+
+const warningButton = {
+    color: '#EC7A7A',
+    backgroundColor: 'rgba(255, 0, 0, .08)',
+    '&:hover': {
+        backgroundColor: 'rgba(255, 0, 0, .05)'
+    }
+}
+
+const pendingButton = {
+    color: '#ecc47a',
+    backgroundColor: 'rgba(255, 204, 0, 0.078)',
+    '&:hover': {
+        backgroundColor: 'rgba(255, 204, 0, 0.5)'
+    }
+}
+
+const successButton = {
+    color: '#7aeca0',
+    backgroundColor: 'rgba(0, 255, 106, 0.078)',
+    '&:hover': {
+        backgroundColor: 'rgba(0, 255, 106, 0.5)'
+    }
+}
+
+export { muiButton, floatRight, paymentButton, warningButton, pendingButton, successButton }
