6cf6ca66bb63fe3c3694a0efb1549d6143441c88
D3NCyan <edwarddoltons@gmail.com>
2021-10-25 01:16:21 +0800

    create privateRoute

--- src/pages/App.js
+++ src/pages/App.js
@@ -3,7 +3,9 @@ import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Logo } from '../assets'
 import { Header, Footer, Modal } from '../components'
+import { ListTransaction } from './'
 import { Home, DetailTrip, Payment, Profile } from './'
+import { PrivateRoute } from '../config'
 
 const App = () => {
     return (
@@ -31,6 +33,7 @@ const App = () => {
                         <Modal variant="modal-login" />
                         <Modal variant="modal-register" />
                     </Route>
+                    <PrivateRoute path="/list-transaction" component={ListTransaction} />
                 </Switch>
             <Footer />
         </Router>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from './Home/Home'
 import DetailTrip from './DetailTrip/DetailTrip'
 import Payment from './Payment/Payment'
 import Profile from './Profile/Profile'
+import ListTransaction from './ListTransaction/ListTransaction'
 
-export { Home, DetailTrip, Payment, Profile }
+export { Home, DetailTrip, Payment, Profile, ListTransaction }
