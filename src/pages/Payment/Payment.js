fa731497785333ee8a438382f9f7c096aa874ed7
D3NCyan <edwarddoltons@gmail.com>
2021-10-24 12:23:03 +0800

    add mui alert effect

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -4,7 +4,36 @@ import { Box, Gap } from '../../components'
 import { setData } from '../../utils' 
 import { useState } from 'react'
 
+// mui component
+import Button from '@mui/material/Button'
+import Snackbar from '@mui/material/Snackbar'
+import IconButton from '@mui/material/IconButton'
+import { Alert } from '@mui/material'
+
 const Payment = () => {
+    // for handle mui alert effect
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => { setOpen(true) }
+    const handleClose = () => { setOpen(false) }
+
+    const action = (
+        <>
+            <Button color="secondary" size="small" onClick={handleClose}>
+                CLOSE
+            </Button>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+            </IconButton>
+        </>
+    )
+    // close
+    
+    
     // get data from api localstorage
     const payment = JSON.parse(localStorage.getItem('payment'))
     const [state, setstate] = useState(payment)
@@ -41,7 +70,22 @@ const Payment = () => {
                     })
 
                     setData('payment', statusPayment())
+                    handleClick()
                 }} />
+
+            <Snackbar sx={{
+                position: 'fixed',
+                bottom: 0,
+                zIndex: 999999999,
+                transform: 'translate(50px, -25px) scale(1.2)',
+                boxShadow: '0 0 50px rgba(0, 0, 0, .26)'
+            }} open={open} autoHideDuration={6000} onClose={handleClose} action={action}>
+
+                <Alert onClose={handleClose} severity='success' sx={{ width: '100%' }}>
+                    Payment success
+                </Alert>
+
+            </Snackbar>
         </div>
     )
 }
