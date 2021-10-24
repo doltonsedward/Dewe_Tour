f4628fb1668015b0db18b6ad507142cf9fd89bc9
D3NCyan <edwarddoltons@gmail.com>
2021-10-24 10:34:14 +0800

    remove link from react

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Logo } from '../assets'
 import { Header, Footer, Modal } from '../components'
 import { Home, DetailTrip, Payment } from './'
