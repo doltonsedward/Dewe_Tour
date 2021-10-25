cd1c843065029d862adbf5813f0b6c24879bd885
D3NCyan <edwarddoltons@gmail.com>
2021-10-26 07:36:56 +0800

    add redirect from react-router-dom

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Home from './Home/Home'
 import DetailTrip from './DetailTrip/DetailTrip'
 import Payment from './Payment/Payment'
 import Profile from './Profile/Profile'
+import NotFound from './Not-Found/NotFound'
 import { ListTransaction, Trip, AddTrip } from './Admin'
 
-export { Home, DetailTrip, Payment, Profile, ListTransaction, Trip, AddTrip }
+export { Home, DetailTrip, Payment, Profile, ListTransaction, Trip, AddTrip, NotFound }
