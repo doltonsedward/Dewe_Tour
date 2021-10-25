534287e00a3bdeca765a096aa56b8a15adb643b0
D3NCyan <edwarddoltons@gmail.com>
2021-10-26 05:43:31 +0800

    add AddTrip component

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,4 +1,5 @@
 import ListTransaction from "./ListTransaction/ListTransaction"
 import Trip from "./Trip/Trip"
+import AddTrip from "./AddTrip/AddTrip"
 
-export { ListTransaction, Trip }
+export { ListTransaction, Trip, AddTrip }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,6 @@ import Home from './Home/Home'
 import DetailTrip from './DetailTrip/DetailTrip'
 import Payment from './Payment/Payment'
 import Profile from './Profile/Profile'
-import { ListTransaction, Trip } from './Admin'
+import { ListTransaction, Trip, AddTrip } from './Admin'
 
-export { Home, DetailTrip, Payment, Profile, ListTransaction, Trip }
+export { Home, DetailTrip, Payment, Profile, ListTransaction, Trip, AddTrip }
