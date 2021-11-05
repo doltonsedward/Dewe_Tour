ea3f38000dbd1f2f67f194ee1bca2870560b8664
D3NCyan <edwarddoltons@gmail.com>
2021-11-05 08:38:23 +0800

    add private styling for trip

--- src/pages/Admin/Trip/Trip.js
+++ src/pages/Admin/Trip/Trip.js
@@ -1,4 +1,4 @@
-import './Trip.scss'
+import './_Trip.scss'
 
 import { useEffect, useState } from 'react'
 import { Gap, Text, Group, Box } from '../../../components'
@@ -15,6 +15,7 @@ const Trip = () => {
     const history = useHistory()
 
     const [dataTrip, setDataTrip] = useState([])
+    const [isActButton, setIsActButton] = useState('hidden')
 
     const getTrips = async () => {
         try {
@@ -29,6 +30,10 @@ const Trip = () => {
         getTrips()
     }, [])
 
+    const actionButton = () => {
+        isActButton === 'hidden' ? setIsActButton('active') : setIsActButton('hidden')
+    }
+
     return (
         <div className="header-default">
             <div className="hero"></div>
@@ -36,17 +41,25 @@ const Trip = () => {
                 <div className="wrapper-list-trip">
                     <Group variant="space-between">
                         <Text variant="h1" fontSize={36}>Income Trip</Text>
-                        <Button variant="contained" sx={muiButton} onClick={()=> history.push('/add-trip')}>add trip</Button>
+                        <div>
+                            <Button variant="contained" sx={muiButton} onClick={()=> history.push('/add-trip')}>add trip</Button>
+                            <Button variant="contained" sx={muiButton} onClick={actionButton}>action</Button>
+                        </div>
                     </Group>
                     <div className="row">
-                        {dataTrip.map((item) => (
-                            <Box key={item.id} className="col-4 col-s-6"
-                                variant="content" 
-                                item={item}
-                                subtext="Australia"
-                                onClick={()=> history.push('/detail-trip/' + item.id)}
-                            />
-                        ))}
+                        {dataTrip.map((item) => {
+                            const wrapperBox = `wrapper-box__trip ${isActButton}`
+                            return (
+                                <div className={wrapperBox}>
+                                    <Box key={item.id} className="col-4 col-s-6"
+                                        variant="content" 
+                                        item={item}
+                                        subtext="Australia"
+                                        onClick={()=> history.push('/detail-trip/' + item.id)}
+                                    />
+                                </div>
+                            )}
+                        )}
                     </div>
                 </div>
             <Gap height={121} />
