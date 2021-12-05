e46066268eedf9c035fdda3a066f857f3dc958de
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:24:32 +0800

    fix warning in Text component

--- src/components/atoms/Text/index.js
+++ src/components/atoms/Text/index.js
@@ -1,21 +1,21 @@
-const Text = ({variant, fontSize, fontWeight, lineHeight, ...rest}) => {
+const Text = ({variant, fontSize, fontWeight, lineHeight, children, ...rest}) => {
     switch (variant) {
         case 'h1':
-            return <h1 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} /> 
+            return <h1 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h1> 
         case 'h2':
-            return <h2 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <h2 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h2> 
         case 'h3':
-            return <h3 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <h3 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h3> 
         case 'h4':
-            return <h4 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <h4 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h4> 
         case 'h5':
-            return <h5 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <h5 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h5> 
         case 'h6':
-            return <h6 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <h6 style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</h6> 
         case 'p':
-            return <p style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <p style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</p> 
         case 'bold':
-            return <b style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest} />
+            return <b style={{fontSize: fontSize, fontWeight: fontWeight, lineHeight: lineHeight}} {...rest}>{children}</b> 
         default:
             throw new Error('Variant not found')
     }
