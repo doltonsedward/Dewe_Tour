968c42cf865864096f9587ae4d358ad119cb3766
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:27:00 +0800

    separate Router and checkUser function

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,101 +1,23 @@
-
 import './App.css'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import { Logo } from '../assets'
-import { Header, Footer } from '../components'
-import { Home, DetailTrip, Payment, Profile, ListTransaction, Trip, AddTrip, NotFound, Search } from './'
-import { API, PrivateRoute, setAuthToken } from '../config'
-import store from '../store'
 import { useEffect } from 'react'
-import { ChatAdmin, Dashboard, UpdateTrip } from './Admin'
-import { useSelector } from 'react-redux'
-import AddCountry from './Admin/AddCountry/AddCountry'
-import Chat from './Chat'
+import { checkUser, Routes, setAuthToken } from '../config'
 
 if (localStorage.token) {
     setAuthToken(localStorage.token)
 }
 
 const App = () => {
-    const currentState = useSelector(state => state)
-    console.log(currentState)
-    
     useEffect(()=> {
         if (localStorage.token) {
             setAuthToken(localStorage.token)
         }
-    }, [currentState])
-
-    const checkUser = async () => {
-        try {
-            const response = await API.get('/check-auth')
-
-            if (response.status === 404) {
-                return store.dispatch({
-                    type: "AUTH_ERROR",
-                });
-            }
-            
-            let payload = response.data.data.user
-            
-            payload.token = localStorage.token;
-
-            store.dispatch({
-                type: "USER_SUCCESS",
-                payload,
-              });
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    }, [])
 
     useEffect(()=> {
         checkUser()
     }, [])
     
-    return (
-        <>
-        <Router>
-            <Header logo={Logo} />
-                <Switch>
-                    <Route exact path="/">
-                        <Home />
-                    </Route>
-                    <Redirect path="/detail-trip/" to="/not-found" exact />
-                    <Route path="/detail-trip/:id">
-                        <DetailTrip />
-                    </Route>
-                    <Route path="/profile">
-                        <Profile />
-                    </Route>
-                    <Route path="/payment">
-                        <Payment />
-                    </Route>
-                    <Route path="/search">
-                        <Search />
-                    </Route>
-                    <Route path="/message">
-                        <Chat />
-                    </Route>
-
-                    <PrivateRoute path="/admin/dashboard" component={Dashboard} />
-                    <PrivateRoute path="/admin/message" component={ChatAdmin} />
-                    <PrivateRoute path="/list-transaction" component={ListTransaction} />
-                    <PrivateRoute path="/trip" component={Trip} />
-                    <PrivateRoute path="/add-country" component={AddCountry} />
-                    <PrivateRoute path="/add-trip" component={AddTrip} />
-                    <PrivateRoute path="/update-trip/:id" component={UpdateTrip} />
-                    
-                    {/* if route is not exist, send default route */}
-                    <Route>
-                        <Redirect to="/not-found" />
-                        <NotFound />
-                    </Route>
-                </Switch>
-            <Footer />
-        </Router>
-        </>
-    )
+    return <Routes />
 }
 
 export default App
