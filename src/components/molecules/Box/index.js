2856667324c8a4d7ec3ce2da6f8b501a283d9369
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:20:22 +0800

    refactor Box component to separate component

--- src/components/molecules/Box/index.js
+++ src/components/molecules/Box/index.js
@@ -1,19 +1,10 @@
 import './_Box.scss'
-import { Gap } from '../../atoms'
 
 // box component
 import ContentBox from './ContentBox'
 import PaymentBox from './PaymentBox'
 import CardBox from './CardBox'
 import PaymentAdminBox from './PaymentAdminBox'
-
-const ProfileBox = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
-}
 
 const DashboardBox = ({heading, text, theme}) => {
     const dashboardClass = `dashboard-box ${theme}`
@@ -25,35 +16,4 @@ const DashboardBox = ({heading, text, theme}) => {
     )
 }
 
-
-const Box = ({variant, theme, logo, heading, text, img, item, setstate, status,  ...rest}) => {
-    switch (variant) {
-        case 'card':
-            return <CardBox logo={logo} heading={heading} text={text} />
-        case 'content':
-            return <ContentBox item={item} {...rest} />
-        case 'payment':
-            return <PaymentBox status={status} item={item} setstate={setstate} {...rest} />
-        case 'payment-admin':
-            return <PaymentAdminBox item={item} setstate={setstate} />
-        case 'profile':
-            return <ProfileBox />
-        case 'box-dashboard':
-            return <DashboardBox heading={heading} theme={theme} text={text} logo={logo} />
-    
-        default:
-            return (
-                <div className="card-box">
-                    <img src={img} alt={heading} />
-                    <Gap height={24} />
-                    <p className="heading__card-box">{heading}</p>
-                    <Gap height={9} />
-                    <p className="subheading__card-box">{text}</p>
-                </div>
-            )
-    }
-
-    
-}
-
-export default Box
+export { CardBox, ContentBox, PaymentBox, PaymentAdminBox, DashboardBox }
