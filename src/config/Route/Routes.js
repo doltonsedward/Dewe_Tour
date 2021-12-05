be423d9f3974e45d93a62dd28d073e3849892cc8
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:22:46 +0800

    separate Routes

---
+++ src/config/Route/Routes.js
@@ -0,0 +1,66 @@
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
+import { Logo } from '../../assets'
+import { Header, Footer } from '../../components'
+import { PrivateRoute } from '../../config'
+import { 
+    Home, 
+    DetailTrip, 
+    Chat, 
+    Dashboard, 
+    Payment, 
+    Profile, 
+    ListTransaction, 
+    Trip, 
+    AddTrip, 
+    NotFound, 
+    Search,
+    ChatAdmin,
+    AddCountry,
+    UpdateTrip
+} from '../../pages'
+
+const Routes = () => {
+    return (
+        <Router>
+            <Header logo={Logo} />
+                <Switch>
+                    <Route exact path="/">
+                        <Home />
+                    </Route>
+                    <Redirect path="/detail-trip/" to="/not-found" exact />
+                    <Route path="/detail-trip/:id">
+                        <DetailTrip />
+                    </Route>
+                    <Route path="/profile">
+                        <Profile />
+                    </Route>
+                    <Route path="/payment">
+                        <Payment />
+                    </Route>
+                    <Route path="/search">
+                        <Search />
+                    </Route>
+                    <Route path="/message">
+                        <Chat />
+                    </Route>
+
+                    <PrivateRoute path="/admin/dashboard" component={Dashboard} />
+                    <PrivateRoute path="/admin/message" component={ChatAdmin} />
+                    <PrivateRoute path="/list-transaction" component={ListTransaction} />
+                    <PrivateRoute path="/trip" component={Trip} />
+                    <PrivateRoute path="/add-country" component={AddCountry} />
+                    <PrivateRoute path="/add-trip" component={AddTrip} />
+                    <PrivateRoute path="/update-trip/:id" component={UpdateTrip} />
+                    
+                    {/* if route is not exist, send default route */}
+                    <Route>
+                        <Redirect to="/not-found" />
+                        <NotFound />
+                    </Route>
+                </Switch>
+            <Footer />
+        </Router>
+    )
+}
+
+export default Routes
