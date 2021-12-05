cac5e0eb68774bdccfde9616db888a2088444294
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:25:07 +0800

    separate box component in molecules

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,9 +1,8 @@
 import Header from './Header'
 import Footer from './Footer'
-import Box from './Box'
-import Modal from './Modal'
 import SidebarAdmin from './SidebarAdmin'
 import Message from './Complain/Message'
 import Contact from './Complain/Contact'
 
-export { Header, Footer, Box, Modal, SidebarAdmin, Message, Contact }
+export * from './Box'
+export { Header, Footer, SidebarAdmin, Message, Contact }
