8068b23a36c8128e9637f1a3f740402c42145a16
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:19:44 +0800

    refactor code, add custom theme for MUI

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {} from 'dotenv/config'
+import 'react-toastify/dist/ReactToastify.css'
 import "./index.css";
 import './assets/css/root.css';
 import './assets/scss/_shortcut.scss';
+import { ToastContainer } from 'react-toastify'
 import App from "./pages/App";
 import store from './store'
 import { Provider } from "react-redux";
-require('dotenv').config()
+
+// import MUI component
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FFAF00',
+      contrastText: "rgba(255, 255, 255, 1)"
+    }
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <ToastContainer position="top-right" />
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
