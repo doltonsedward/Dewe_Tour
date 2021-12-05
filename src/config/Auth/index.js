1b564dedf4cea0488c52d90d6abca05f203c56ef
D3NCyan <edwarddoltons@gmail.com>
2021-12-05 19:26:22 +0800

    separate checkUser function

---
+++ src/config/Auth/index.js
@@ -0,0 +1,27 @@
+import { API } from '..';
+import store from '../../store'
+
+const checkUser = async () => {
+    try {
+        const response = await API.get('/check-auth')
+
+        if (response.status !== 200) {
+            return store.dispatch({
+                type: "AUTH_ERROR",
+            });
+        }
+        
+        let payload = response.data.data.user
+        
+        payload.token = localStorage.token;
+
+        store.dispatch({
+            type: "USER_SUCCESS",
+            payload,
+          });
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export { checkUser }
