32aa9a4586a9cceab190be9e62772f236a979e1d
D3NCyan <edwarddoltons@gmail.com>
2021-12-06 21:32:03 +0800

    add notif when error or success

--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,8 +1,8 @@
 import './Payment.scss'
-
 import { PaymentBox, Gap } from '../../components'
 import { useEffect, useState } from 'react'
 import { ImageEmpty3D } from '../../assets'
+import { toast } from 'react-toastify'
 
 // import api
 import { API } from '../../config'
@@ -18,7 +18,8 @@ const Payment = () => {
             const response = await API.get('/transaction')
             setDataTrans(response.data.data)
         } catch (error) {
-            console.log(error)
+            const message = error?.response?.data?.message || error?.message
+            toast.error(message || 'Unknow error')
         }
     }
 
@@ -32,24 +33,29 @@ const Payment = () => {
     return (
         <div className="payment header-default">
             <div className="hero"></div>
-            <Gap height={50} />
             {
                 !waitingPayment.length ? 
-                <p className="text-center"><img style={{width: '450px', maxWidth: '90%'}} src={ImageEmpty3D} alt="" /></p>
+                <p className="text-center">
+                    <img style={{width: '400px', maxWidth: '90%'}} src={ImageEmpty3D} alt="" />
+                    <p>Your payment is empty right now</p>
+                </p>
                 :
-                waitingPayment.map(item => {
-                    return (
-                        <PaymentBox 
-                            name={item?.trip?.title} 
-                            country={item?.trip.country?.name}
-                            type={item?.trip?.type}  
-                            count={item?.counterQty} 
-                            status={item.status}
-                            item={item} 
-                            fetching={getTransaction}
-                        />
-                    )
-                })
+                <>
+                    <Gap height={50} />
+                    {waitingPayment.map(item => {
+                        return (
+                            <PaymentBox 
+                                name={item?.trip?.title} 
+                                country={item?.trip.country?.name}
+                                type={item?.trip?.type}  
+                                count={item?.counterQty} 
+                                status={item.status}
+                                item={item} 
+                                fetching={getTransaction}
+                            />
+                        )
+                    })}
+                </>
             }
         </div>
     )
