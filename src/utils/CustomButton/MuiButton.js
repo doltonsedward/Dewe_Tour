0e43e98c26fbbf7a9b7f514fb3c1c9c155a92f35
D3NCyan <edwarddoltons@gmail.com>
2022-01-15 19:21:41 +0800

    DEWE-2 clean code in CustomButton

--- src/utils/CustomButton/MuiButton.js
+++ src/utils/CustomButton/MuiButton.js
@@ -1,71 +1,81 @@
 const muiButton = {
-    backgroundColor: '#FFAF00',
-    '&:hover': {
-        backgroundColor: '#e7bb5a'
-    }
-}
+  backgroundColor: "#FFAF00",
+  "&:hover": {
+    backgroundColor: "#e7bb5a",
+  },
+};
 
 const floatRight = {
-    backgroundColor: '#FFAF00',
-    '&:hover': {
-        backgroundColor: '#e7bb5a'
-    },
-}
+  backgroundColor: "#FFAF00",
+  "&:hover": {
+    backgroundColor: "#e7bb5a",
+  },
+};
 
 const paymentButton = {
-    width: '213px',
-    height: '50px',
-    backgroundColor: '#FFAF00',
-    '&:hover': {
-        backgroundColor: '#e7bb5a'
-    }
-}
+  width: "213px",
+  height: "50px",
+  backgroundColor: "#FFAF00",
+  "&:hover": {
+    backgroundColor: "#e7bb5a",
+  },
+};
 
 const profileCoverButton = {
-    width: '100%',
-    height: '50px',
-    backgroundColor: '#FFAF00',
-    '&:hover': {
-        backgroundColor: '#e7bb5a'
-    }
-}
+  width: "100%",
+  height: "50px",
+  backgroundColor: "#FFAF00",
+  "&:hover": {
+    backgroundColor: "#e7bb5a",
+  },
+};
 
 const warningButton = {
-    color: '#EC7A7A',
-    backgroundColor: 'rgba(255, 0, 0, .08)',
-    '&:hover': {
-        backgroundColor: 'rgba(255, 0, 0, .05)'
-    }
-}
+  color: "#EC7A7A",
+  backgroundColor: "rgba(255, 0, 0, .08)",
+  "&:hover": {
+    backgroundColor: "rgba(255, 0, 0, .05)",
+  },
+};
 
 const pendingButton = {
-    color: '#ecc47a',
-    backgroundColor: 'rgba(255, 204, 0, 0.078)',
-    '&:hover': {
-        backgroundColor: 'rgba(255, 204, 0, .05)'
-    }
-}
+  color: "#ecc47a",
+  backgroundColor: "rgba(255, 204, 0, 0.078)",
+  "&:hover": {
+    backgroundColor: "rgba(255, 204, 0, .05)",
+  },
+};
 
 const successButton = {
-    color: '#7aeca0',
-    backgroundColor: 'rgba(0, 255, 106, 0.078)',
-    '&:hover': {
-        backgroundColor: 'rgba(0, 255, 106, .05)'
-    }
-}
+  color: "#7aeca0",
+  backgroundColor: "rgba(0, 255, 106, 0.078)",
+  "&:hover": {
+    backgroundColor: "rgba(0, 255, 106, .05)",
+  },
+};
 
 const greenButton = {
-    backgroundColor: '#0ACF83',
-    '&:hover': {
-        backgroundColor: '#44d39c'
-    }
-}
+  backgroundColor: "#0ACF83",
+  "&:hover": {
+    backgroundColor: "#44d39c",
+  },
+};
 
 const redButton = {
-    backgroundColor: '#FF0742',
-    '&:hover': {
-        backgroundColor: '#e93962'
-    }
-}
+  backgroundColor: "#FF0742",
+  "&:hover": {
+    backgroundColor: "#e93962",
+  },
+};
 
-export { muiButton, floatRight, paymentButton, profileCoverButton, warningButton, pendingButton, successButton, redButton, greenButton }
+export {
+  muiButton,
+  floatRight,
+  paymentButton,
+  profileCoverButton,
+  warningButton,
+  pendingButton,
+  successButton,
+  redButton,
+  greenButton,
+};
