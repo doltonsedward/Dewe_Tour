6ccac1828a445dc858c7cb40ef4d2cffbff47506
D3NCyan <edwarddoltons@gmail.com>
2022-01-18 10:34:10 +0800

    DEWE-10 done clean up code in assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,60 +1,91 @@
-import Logo from './icon/logo.png';
-import LogoSecond from './icon/logo-2.png';
+import Logo from "./icon/logo.png";
+import LogoSecond from "./icon/logo-2.png";
 
 // icon for box
-import LogoGuarante from './icon/guarantee.svg'
-import LogoHeart from './icon/heart.svg'
-import LogoAgent from './icon/agent.svg'
-import LogoSupport from './icon/support.svg'
-import IconPalm from './icon/palm.png'
-import IconHibicus from './icon/hibicus.png'
+import LogoGuarante from "./icon/guarantee.svg";
+import LogoHeart from "./icon/heart.svg";
+import LogoAgent from "./icon/agent.svg";
+import LogoSupport from "./icon/support.svg";
+import IconPalm from "./icon/palm.png";
+import IconHibicus from "./icon/hibicus.png";
 
 // icon for login register
-import IconPalm2 from './icon/palm1.png'
-import IconHibicus2 from './icon/hibicus1.png'
-import IconUser from './icon/user.svg'
-import IconBill from './icon/bill.svg'
-import IconLogout from './icon/logout.svg'
-import IconTrip from './icon/journey.svg'
+import IconPalm2 from "./icon/palm1.png";
+import IconHibicus2 from "./icon/hibicus1.png";
+import IconUser from "./icon/user.svg";
+import IconBill from "./icon/bill.svg";
+import IconLogout from "./icon/logout.svg";
+import IconTrip from "./icon/journey.svg";
 
 // icon for footer
-import IconLeaf from './icon/leaf.png'
+import IconLeaf from "./icon/leaf.png";
 
 // icon for detail-trip
-import IconHotel from './icon/hotel.svg'
-import IconPlane from './icon/plane.svg'
-import IconMeal from './icon/meal.svg'
-import IconTime from './icon/time.svg'
-import IconCalendar from './icon/calendar.svg'
+import IconHotel from "./icon/hotel.svg";
+import IconPlane from "./icon/plane.svg";
+import IconMeal from "./icon/meal.svg";
+import IconTime from "./icon/time.svg";
+import IconCalendar from "./icon/calendar.svg";
 
 // img for profile
-import ProfileDefault from './img/profile/radif.jpg'
-import DataTour from './json'
-import IconUserCircle from './icon/user-circle.png'
-import IconEmail from './icon/email.png'
-import IconPhone from './icon/phone.png'
-import IconLocation from './icon/location.png'
+import ProfileDefault from "./img/profile/radif.jpg";
+import DataTour from "./json";
+import IconUserCircle from "./icon/user-circle.png";
+import IconEmail from "./icon/email.png";
+import IconPhone from "./icon/phone.png";
+import IconLocation from "./icon/location.png";
 
 // img for payment
-import ImgTransfer from './img/transfer/tf-1.jpg'
-import ImgQrCode from './img/qr-code/qr-code1.svg'
+import ImgTransfer from "./img/transfer/tf-1.jpg";
+import ImgQrCode from "./img/qr-code/qr-code1.svg";
 
-import ImgCamera from './img/camera.jpg'
+import ImgCamera from "./img/camera.jpg";
 
-import IconSearch from './icon/search.svg'
-import IconFile from './icon/file.svg'
-import ImageEmpty from './img/empty-image.svg'
-import ImageEmpty3D from './img/empty-image.png'
+import IconSearch from "./icon/search.svg";
+import IconFile from "./icon/file.svg";
+import ImageEmpty from "./img/empty-image.svg";
+import ImageEmpty3D from "./img/empty-image.png";
 
 // image for httprequest error
-import ImgPageNotFound from './img/not-found.svg'
+import ImgPageNotFound from "./img/not-found.svg";
 
 // image for empty message
-import ImgInboxCleanUp from './img/inbox-cleanup.svg'
+import ImgInboxCleanUp from "./img/inbox-cleanup.svg";
 
-export { Logo, LogoSecond, LogoGuarante, LogoHeart, LogoAgent, LogoSupport, IconPalm, IconHibicus, IconTrip }
-export { IconPalm2, IconHibicus2, IconLeaf, IconUser, IconBill, IconLogout, IconHotel, IconPlane, IconMeal, IconTime, IconCalendar }
-export { ProfileDefault }
-export { ImgTransfer, ImgQrCode, ImgPageNotFound, ImgCamera, ImgInboxCleanUp }
-export { DataTour }
-export { IconUserCircle, IconEmail, IconPhone, IconLocation, IconSearch, IconFile, ImageEmpty, ImageEmpty3D }
+export {
+  Logo,
+  LogoSecond,
+  LogoGuarante,
+  LogoHeart,
+  LogoAgent,
+  LogoSupport,
+  IconPalm,
+  IconHibicus,
+  IconTrip,
+};
+export {
+  IconPalm2,
+  IconHibicus2,
+  IconLeaf,
+  IconUser,
+  IconBill,
+  IconLogout,
+  IconHotel,
+  IconPlane,
+  IconMeal,
+  IconTime,
+  IconCalendar,
+};
+export { ProfileDefault };
+export { ImgTransfer, ImgQrCode, ImgPageNotFound, ImgCamera, ImgInboxCleanUp };
+export { DataTour };
+export {
+  IconUserCircle,
+  IconEmail,
+  IconPhone,
+  IconLocation,
+  IconSearch,
+  IconFile,
+  ImageEmpty,
+  ImageEmpty3D,
+};
--- src/assets/json/index.js
+++ src/assets/json/index.js
@@ -1,3 +1,3 @@
-import DataTour from './list-tour.json'
+import DataTour from "./list-tour.json";
 
-export default DataTour
+export default DataTour;
