2d37ca1465d7df51a0fd5ea2522a27ea7d2bc58e
D3NCyan <edwarddoltons@gmail.com>
2022-01-18 10:29:10 +0800

    done clean up code in src

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {} from 'dotenv/config'
-import 'react-toastify/dist/ReactToastify.css'
+import {} from "dotenv/config";
+import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
-import './assets/css/root.css';
-import './assets/scss/_shortcut.scss';
-import { ToastContainer } from 'react-toastify'
+import "./assets/css/root.css";
+import "./assets/scss/_shortcut.scss";
+import { ToastContainer } from "react-toastify";
 import App from "./pages/App";
-import store from './store'
+import store from "./store";
 import { Provider } from "react-redux";
 
 // import MUI component
-import { createTheme, ThemeProvider } from '@mui/material/styles'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#FFAF00',
-      contrastText: "rgba(255, 255, 255, 1)"
-    }
-  }
-})
+      main: "#FFAF00",
+      contrastText: "rgba(255, 255, 255, 1)",
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
