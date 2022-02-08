d5b7e44d024cb5c4256a2fa0a84adbb409d3ff3b
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-08 19:39:02 +0800

    fix loading in routes

--- src/config/Route/Routes.js
+++ src/config/Route/Routes.js
@@ -29,18 +29,20 @@ import {
 
 const Routes = () => {
   const currentState = useSelector((state) => state);
-  return currentState.isLoading ? (
-    <div className="loading-section">
-      <div className="loading">
-        <p>loading</p>
-        <span></span>
-      </div>
-    </div>
-  ) : (
+
+  return (
     <Router>
       <Header logo={Logo} />
       <Switch>
         <Route exact path="/">
+          {currentState.isLoading && (
+            <div className="loading-section">
+              <div className="loading">
+                <p>loading</p>
+                <span></span>
+              </div>
+            </div>
+          )}
           <Home />
         </Route>
         <Redirect path="/detail-trip/" to="/not-found" exact />
@@ -77,6 +79,54 @@ const Routes = () => {
       <Footer />
     </Router>
   );
+  // return currentState.isLoading ? (
+  //   <div className="loading-section">
+  //     <div className="loading">
+  //       <p>loading</p>
+  //       <span></span>
+  //     </div>
+  //   </div>
+  // ) : (
+  //   <Router>
+  //     <Header logo={Logo} />
+  //     <Switch>
+  //       <Route exact path="/">
+  //         <Home />
+  //       </Route>
+  //       <Redirect path="/detail-trip/" to="/not-found" exact />
+  //       <Route path="/detail-trip/:id">
+  //         <DetailTrip />
+  //       </Route>
+  //       <Route path="/profile">
+  //         <Profile />
+  //       </Route>
+  //       <Route path="/payment">
+  //         <Payment />
+  //       </Route>
+  //       <Route path="/search">
+  //         <Search />
+  //       </Route>
+  //       <Route path="/message">
+  //         <Chat />
+  //       </Route>
+
+  //       <PrivateRoute path="/admin/dashboard" component={Dashboard} />
+  //       <PrivateRoute path="/admin/message" component={ChatAdmin} />
+  //       <PrivateRoute path="/list-transaction" component={ListTransaction} />
+  //       <PrivateRoute path="/trip" component={Trip} />
+  //       <PrivateRoute path="/add-country" component={AddCountry} />
+  //       <PrivateRoute path="/add-trip" component={AddTrip} />
+  //       <PrivateRoute path="/update-trip/:id" component={UpdateTrip} />
+
+  //       {/* if route is not exist, send default route */}
+  //       <Route>
+  //         <Redirect to="/not-found" />
+  //         <NotFound />
+  //       </Route>
+  //     </Switch>
+  //     <Footer />
+  //   </Router>
+  // );
 };
 
 export default Routes;
