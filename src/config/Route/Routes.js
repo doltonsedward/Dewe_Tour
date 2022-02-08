2ade3e46c6d29f9e692ae3a98d640fdb509eff81
Doltons Edward Nicholas <75367447+doltonsedward@users.noreply.github.com>
2022-02-08 19:40:39 +0800

    remove useless code in routes

--- src/config/Route/Routes.js
+++ src/config/Route/Routes.js
@@ -79,54 +79,6 @@ const Routes = () => {
       <Footer />
     </Router>
   );
-  // return currentState.isLoading ? (
-  //   <div className="loading-section">
-  //     <div className="loading">
-  //       <p>loading</p>
-  //       <span></span>
-  //     </div>
-  //   </div>
-  // ) : (
-  //   <Router>
-  //     <Header logo={Logo} />
-  //     <Switch>
-  //       <Route exact path="/">
-  //         <Home />
-  //       </Route>
-  //       <Redirect path="/detail-trip/" to="/not-found" exact />
-  //       <Route path="/detail-trip/:id">
-  //         <DetailTrip />
-  //       </Route>
-  //       <Route path="/profile">
-  //         <Profile />
-  //       </Route>
-  //       <Route path="/payment">
-  //         <Payment />
-  //       </Route>
-  //       <Route path="/search">
-  //         <Search />
-  //       </Route>
-  //       <Route path="/message">
-  //         <Chat />
-  //       </Route>
-
-  //       <PrivateRoute path="/admin/dashboard" component={Dashboard} />
-  //       <PrivateRoute path="/admin/message" component={ChatAdmin} />
-  //       <PrivateRoute path="/list-transaction" component={ListTransaction} />
-  //       <PrivateRoute path="/trip" component={Trip} />
-  //       <PrivateRoute path="/add-country" component={AddCountry} />
-  //       <PrivateRoute path="/add-trip" component={AddTrip} />
-  //       <PrivateRoute path="/update-trip/:id" component={UpdateTrip} />
-
-  //       {/* if route is not exist, send default route */}
-  //       <Route>
-  //         <Redirect to="/not-found" />
-  //         <NotFound />
-  //       </Route>
-  //     </Switch>
-  //     <Footer />
-  //   </Router>
-  // );
 };
 
 export default Routes;
